26a3deae73ea4280278f4f9b46553944fe7ef4d6
majestic-ad <majestic_ad@protonmail.com>
2021-05-21 12:34:25 +0300

    First HomeWork

--- app/src/main/java/com/example/homeworkmovie/MainActivity.kt
+++ app/src/main/java/com/example/homeworkmovie/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.homeworkmovie
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.homeworkmovie.databinding.ActivityMainBinding
+
+lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button2.setOnClickListener{
+            startActivity(Intent(this, MovieDetailsActivity::class.java))
+        }
     }
+
 }
